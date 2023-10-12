aee530310b93b32c3e4905066fd65af8a22e426f
Silent Garden <silentgarden837@gmail.com>
2023-10-12 13:58:04 +0530

    window intialization done

--- printer-test.py
+++ printer-test.py
@@ -1,12 +1,17 @@
 import PySimpleGUI as sg
 
+#######################
+#Window Initialization#
+#######################
+
 sg.theme('DarkAmber')
 
-layout = [[sg.Button('1'), sg.Button('2'), sg.Button('3')]]
+layout = [[sg.Button('1', font='Kanit\ Black 35', size=(2, 1)), sg.Button('2', font='Kanit\ Black 35', size=(2, 1)), sg.Button('3', font='Kanit\ Black 35', size=(2, 1))]]
 
-window = sg.Window('Printer Test', layout, size=(310, 90), resizable=False)
+window = sg.Window('Printer Test', layout, size=(310, 90), resizable=False, element_justification='c')
 
 while True:
 	event, values = window.read()
 	if event == sg.WIN_CLOSED or event == 'Cancel':
 		break
+
